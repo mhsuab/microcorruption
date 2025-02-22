e751a5d96fc09500d3c86d1aeff07eb46f57ceab
5w1Min <mhsuab1346@gmail.com>
2025-02-22 18:36:00 -0500

    [update] rename folder

--- tools/asm.js
+++ tools/asm.js
@@ -1,2 +1,2 @@
 // get all the instructions from the asm box
-diasm = Array.from(document.getElementById('asmbox').childNodes).map((c) => c.getElementsByTagName('pre')[0].innerText)
+diasm = Array.from(document.getElementById('asmbox').childNodes).map((c) => c.getElementsByTagName('pre')[0].innerText); console.log(diasm.join('\n'))
